edb7e4910a37cbbc56a9c31fe3710e6d3225c48b
ComplexUsernames <349462549@gapps.yrdsb.ca>
2025-04-16 09:08:45 -0400

    Add rock

--- MyWorld.java
+++ MyWorld.java
@@ -3,7 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
         Rocket rocket = new Rocket();
         addObject(rocket, 100, 100);
+        
+        Rock rock = new Rock();
+        addObject(rock, 600, 100);
     }
 }
